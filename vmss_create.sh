02d3bb0d0922f1579d330eb50fca8a6dbbf15415
Kenneth Heafield <github@kheafield.com>
2017-04-19 11:39:05 +0100

    Fix bash expression

--- vmss_create.sh
+++ vmss_create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [ "$1" != "--resource-group" && "$1" != "-g" ] || [ "$3" != "--name" && "$3" != "-n" ]; then
+if ([ "$1" != "--resource-group" ] && [ "$1" != "-g" ]) || ([ "$3" != "--name" ] && [ "$3" != "-n" ]); then
   cat 1>&2 <<EOF
 Usage: $0 --resource-group group --name name ...
 All arguments are forwarded to az vmss create, so see that for help.
