94e5e42bfae63a3b0debc02da05a911414fb93e7
Kenneth Heafield <github@kheafield.com>
2017-04-19 17:19:16 +0100

    Fix delete

--- internal/vmss_setup.sh
+++ internal/vmss_setup.sh
@@ -6,11 +6,11 @@ port=$(cut -d : -f 2 <<<$1)
 host=$(cut -d : -f 1 <<<$1)
 shift
 #Accept host keys :-(
-if [ ! ssh-keyscan -p $port $host >>~/.ssh/known_hosts 2>/dev/null ]; then
+if ! ssh-keyscan -p $port $host >>~/.ssh/known_hosts 2>/dev/null; then
   echo $host:$port does not seem to be up, might be hallucinated 1>&2
   exit 1
 fi
 #Copy azure credentials over and make delete/deallocate commands
-(echo $@ --instance-ids '$(hostname |tail -c 7)'; cd ~; tar c .azure) |ssh -p $port $host 'read a; echo -e '\''#!/bin/bash\n'\''az vmss deallocate $a >deallocate && echo -e '\''#!/bin/bash\n'\''az vmss delete $a >delete && chmod +x deallocate delete && tar x && sudo mv deallocate delete /bin'
+(echo $@ --instance-ids '$(hostname |tail -c 7)'; cd ~; tar c .azure) |ssh -p $port $host 'read a; echo -e '\''#!/bin/bash\n'\''az vmss deallocate $a >deallocate && echo -e '\''#!/bin/bash\n'\''az vmss delete-instances $a >delete && chmod +x deallocate delete && tar x && sudo mv deallocate delete /bin'
 #Print hosts in GNU parallel format
 echo 1/ssh -p $port $host
