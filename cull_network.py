8302fa46d946c24e6fc8f17c046fa8d3c4ebbe3d
Kenneth Heafield <github@kheafield.com>
2017-04-27 20:56:01 +0100

    Script to delete network resources that aren't bound to VMs.

---
+++ cull_network.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+#Print commands to delete load balancers, NICs, and IPs not attached to any VM
+#Plenty of bugs here.  Assumes a stock setup with nothing fancy.
+import json
+import subprocess
+
+def query(command):
+  return json.load(subprocess.Popen(command, stdout=subprocess.PIPE).stdout)
+
+lb = query(["az", "network", "lb", "list", "-o", "json"])
+vmss = query(["az", "vmss", "list", "-o", "json"])
+
+in_use_lbs = ['/'.join(m["virtualMachineProfile"]["networkProfile"]["networkInterfaceConfigurations"][0]["ipConfigurations"][0]["loadBalancerBackendAddressPools"][0]["id"].split('/')[0:9]) for m in vmss]
+allocated_lbs = ['/'.join(l["frontendIpConfigurations"][0]["id"].split('/')[0:9]) for l in lb]
+
+unused_lbs = set(allocated_lbs) - set(in_use_lbs)
+for l in unused_lbs:
+  split = l.split('/')
+  print "az network lb delete -g " +  split[4] +  " -n " + split[8]
+
+#TODO: exclude the lbs that are to be deleted
+ip_used_by_lb = [l["frontendIpConfigurations"][0]["publicIpAddress"]["id"] for l in lb]
+
+
+vm = query(["az", "vm", "list", "-o", "json"])
+in_use_nics = [v["networkProfile"]["networkInterfaces"][0]["id"] for v in vm]
+nics = query(["az", "network", "nic", "list", "-o", "json"])
+allocated_nics = [n["id"] for n in nics]
+for n in set(allocated_nics) - set(in_use_nics):
+  split = n.split('/')
+  print "az network nic delete -g " + split[4] + " -n " + split[8]
+
+#TODO: exclude the nics that are to be deleted
+ip_used_by_nic = [n["ipConfigurations"][0]["publicIpAddress"]["id"] for n in nics]
+in_use_ips = ip_used_by_lb + ip_used_by_nic
+
+allocated_ips = [i["id"] for i in query(["az", "network", "public-ip", "list", "-o", "json"])]
+for i in set(allocated_ips) - set(in_use_ips):
+  split = i.split('/')
+  print "az network public-ip delete -g " + split[4] + " -n " + split[8]
+
